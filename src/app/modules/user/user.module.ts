158084501aa79533c7af093451c81e73794a6ffb
alianmc1988 <alianmc1988@gmail.com>
2021-04-06 20:48:36 -0400

    intalled angular schedule y creado componente calendario

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
+
 import { UserEmitterComponent } from './user-emitter/user-emitter.component';
 import { UserReceiverComponent } from './user-receiver/user-receiver.component';
 import { SessionPageComponent } from './session/session-page/session-page.component';
@@ -11,7 +12,7 @@ import { SessionNavbarComponent } from './session/session-navbar/session-navbar.
 import { SlideMenuDirective } from './session/slide-menu.directive';
 import { SessionMainDisplayComponent } from './session/session-main-display/session-main-display.component';
 import { HouseBoockingComponent } from './house-boocking/house-boocking.component';
-import { CalendarComponent } from './session/calendar/calendar.component';
+import { CalendarioComponent } from './session/calendario/calendario.component';
 
 @NgModule({
   declarations: [
@@ -20,8 +21,7 @@ import { CalendarComponent } from './session/calendar/calendar.component';
   SessionPageComponent, 
   SideMenuComponent, 
   FooterComponent, 
-  SessionNavbarComponent, SlideMenuDirective, SessionMainDisplayComponent, HouseBoockingComponent, CalendarComponent
-  ],
+  SessionNavbarComponent, SlideMenuDirective, SessionMainDisplayComponent, HouseBoockingComponent, CalendarioComponent],
   imports: [
     CommonModule,
     UserRoutingModule
